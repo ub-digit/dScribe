4bd93305358aa2df1596ec026ee730922110efa6
jkcarlsson85 <jkcarlsson85@gmail.com>
2015-05-23 10:24:17 +0200

    Add lightbox and selection manipulation logic

--- app/components/active-image.js
+++ app/components/active-image.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   tagName: 'canvas',
   classNames: ['active-image'],
   height: 700,
-  selection: {x: 200, y: 200, h: 200, w: 200},
+  activeFrame: false,
 
   didInsertElement: function(){
     var that = this;
@@ -23,19 +23,18 @@ export default Ember.Component.extend({
 
     // Runs when image is loaded
     promise.then(function(image){
-      that.set('image', image)
-      var image = that.get('image');
-      var scaleratio = image.height/700;
+      that.set('image', image);
+      that.set('item.selection', {x: 0, y: 0, h: image.height, w: image.width});
       Ember.run.scheduleOnce('afterRender', this, function() {
       // creating canvas and context objects
-        that._renderCanvas();
+        //that._renderCanvas();
       });
     });
   },
-  _renderCanvas: function(){
+  draw: function(){
     var image = this.get('image');
     var scaleratio = image.height/this.get('height');
-    var selection = this.get('selection');
+    var selection = this.get('item.selection');
     var canvas = this.get('element');
     var ctx = canvas.getContext('2d');
   
@@ -54,11 +53,92 @@ export default Ember.Component.extend({
     ctx.strokeRect(selection.x/scaleratio, selection.y/scaleratio, selection.w/scaleratio, selection.h/scaleratio);
     // draw part of original image
     ctx.drawImage(image, selection.x, selection.y, selection.w, selection.h,selection.x/scaleratio, selection.y/scaleratio, selection.w/scaleratio, selection.h/scaleratio);
-  },
+
+    // Draw active stuff
+    if (this.get('activeFrame')){
+      ctx.strokeRect((selection.x/scaleratio)-1, (selection.y/scaleratio)-1, (selection.w/scaleratio)+2, (selection.h/scaleratio)+2);
+    }
+  }.observes('image', 'item.selection.x', 'item.selection.y', 'item.selection.h', 'item.selection.w', 'height', 'activeFrame'),
   willDestroyElement: function(){
-  //console.log('Destroying elememnt');
+    //console.log('Destroying elememnt');
   },
   mouseEnter: function(){
-    console.log('mouse over ', this.get('item'));
+    this.set('activeFrame', true);
+    //this._renderCanvas();
+    return this.$().attr({ tabindex: 1 }), this.$().focus();
+  },
+  mouseLeave: function(){
+    this.set('activeFrame', false);
+    //this._renderCanvas();
+  },
+
+  keyDown: function(e) {
+    var that = this;
+    e.preventDefault();
+    
+    switch(e.keyCode) {
+    case 38:
+      that._moveSelectionUp();
+      break;
+    case 40:
+      that._moveSelectionDown();
+      break;
+    case 37:
+      that._moveSelectionLeft();
+      break;
+    case 39:
+      that._moveSelectionRight();
+      break;
+    case 102:
+      that._expandSelectionHorizontal(6);
+      break;
+    case 100:
+      that._reduceSelectionHorizontal(6);
+      break;
+    case 104:
+      that._expandSelectionVertical(6);
+      break;
+    case 98:
+      that._reduceSelectionVertical(6);
+      break;
+    default:
+        console.log('keyPress', this.get('item.title'), e.keyCode);
+    } 
+  },
+
+  _moveSelectionUp: function(){
+    this.decrementProperty('item.selection.y');
+  },
+
+  _moveSelectionDown: function(){
+    this.incrementProperty('item.selection.y');
+  },
+
+  _moveSelectionLeft: function(){
+    this.decrementProperty('item.selection.x');
+  },
+
+  _moveSelectionRight: function(){
+    this.incrementProperty('item.selection.x');
+  },
+
+  _expandSelectionHorizontal: function(spread){
+    this.decrementProperty('item.selection.x', parseInt(spread/2));
+    this.incrementProperty('item.selection.w', spread);
+  },
+
+  _reduceSelectionHorizontal: function(spread){
+    this.incrementProperty('item.selection.x', parseInt(spread/2));
+    this.decrementProperty('item.selection.w', spread);
+  },
+
+  _expandSelectionVertical: function(spread){
+    this.decrementProperty('item.selection.y', parseInt(spread/2));
+    this.incrementProperty('item.selection.h', spread);
+  },
+
+  _reduceSelectionVertical: function(spread){
+    this.incrementProperty('item.selection.y', parseInt(spread/2));
+    this.decrementProperty('item.selection.h', spread);
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -7,7 +7,11 @@ export default Ember.Route.extend({
       var str = "" + i
       var pad = "0000"
       var str = pad.substring(0, pad.length - str.length) + str
-      list.push({small: "images/sample_jobs/job1/web_small/" + str + ".jpg", tiny: "images/sample_jobs/job1/web_tiny/" + str + ".jpg", title: str});
+      list.push({
+        small: "images/sample_jobs/job1/web_small/" + str + ".jpg", 
+        tiny: "images/sample_jobs/job1/web_tiny/" + str + ".jpg", title: str,
+        selection: {x: 200, y: 200, w: 200, h: 200}
+      });
     }
     return list;
   },
